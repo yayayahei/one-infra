6bf8464c47769a0a52485c41db56355012e95b26
yayayahei <yayayahei@outlook.com>
2025-06-26 14:39:11 +0800

    feat(monitor): check node_exporter directory before download
    Change-Id: I36de382c24ea9fb860773e5bb6d89d6ed6eb09dc

--- monitor/host/run_node_exporter.sh
+++ monitor/host/run_node_exporter.sh
@@ -1,8 +1,11 @@
 version=1.9.1
 
+if [ ! -d node_exporter ]; then
 
-curl -L https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.darwin-arm64.tar.gz -o node_exporter.tar.gz -m 20
-tar xvfz node_exporter.tar.gz
-mv node_exporter-$version.darwin-arm64 node_exporter
+  curl -L https://github.com/prometheus/node_exporter/releases/download/v$version/node_exporter-$version.darwin-arm64.tar.gz -o node_exporter.tar.gz -m 20
+  tar xvfz node_exporter.tar.gz
+  mv node_exporter-$version.darwin-arm64 node_exporter
+fi
+
 cd node_exporter
 ./node_exporter --web.listen-address=":9100"
